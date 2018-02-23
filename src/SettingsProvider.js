323408200499fbdd0fe3532ced3f37b29483932c
raideer <idmania1@gmail.com>
2018-02-23 18:33:52 +0200

    Overriding super get set

--- src/SettingsProvider.js
+++ src/SettingsProvider.js
@@ -6,4 +6,12 @@ module.exports = class SettingsProvider extends SequelizeProvider {
             idColumn: 'name'
         });
     }
+
+    set(guild, setting, value) {
+        return super.set(`${setting}:${guild.id}`, 'data', value);
+    }
+
+    get(guild, setting, def) {
+        return super.get(`${setting}:${guild.id}`, 'data', def);
+    }
 };
