91e10e99d4b2c3bf5efb168fe594f38c13f7d7d6
raideer <idmania1@gmail.com>
2018-05-02 01:50:14 +0300

    Number formatting

--- src/Utils.js
+++ src/Utils.js
@@ -8,4 +8,17 @@ module.exports = class Utils {
             'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing',
             'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecrafting', 'Hunter', 'Construction'];
     }
+
+    formatNumber(num) {
+        return Number(num).toLocaleString();
+    }
+
+    formatNumberWithSign(num) {
+        num = Number(num);
+        let str = num.toLocaleString();
+        if (num > 0) {
+            str = `+${str}`;
+        }
+        return str;
+    }
 };
